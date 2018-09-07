ef754d5e95234112801a5a8d5d4e435ba56d7823
benmelnick <bmelnick3@gatech.edu>
2018-09-07 01:35:46 -0400

    Person5 finished

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Dylan Luke
  *  @version 1.1
  */
 public class Person5 {
